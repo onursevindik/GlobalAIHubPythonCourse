7fe2c34a387ea26ddb31c1042388e91868038480
Onur Sevindik <e203712@metu.edu.tr>
2021-04-11 21:52:47 +0300

    Update HW2-Day3.py

--- Homework/HW2/HW2-Day3.py
+++ Homework/HW2/HW2-Day3.py
@@ -1,1 +1,31 @@
+d = {"admin":"admin"}
+print("Welcome!")
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+for k,v in d.items():
+    if (k != username and v == password):
+        print(" ")
+        print("xxXxx Wrong username! xxXxx")
+    elif(k == username and v != password):
+        print(" ")
+        print("xxXxx Wrong password! xxXxx")
+    elif(k != username and v != password):
+        print(" ")
+        print("xxXxx Wrong username and password! xxXxx")
+    else:
+        print(" ")
+        print("Congratulations")
+        print("Login successful!", u'\u2713')
+        
 
+
+
+"""
+for k,v in d.items():
+    print("########################")
+    print("#######--HINT--#########")
+    print("########################")
+    print("    username: ", k)
+    print("    password: ", v)
+    print("########################")
+"""
